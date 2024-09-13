f997411e3d079e0f042a7e14a98e93ca9995429c
klu_2200031618 <kk258786@gmail.com>
2024-09-13 22:56:59 +0530

    Employee SignUp with backend logic has been completed, need to complete login and home page of Employee

--- FrontendMicroservices/src/App.js
+++ FrontendMicroservices/src/App.js
@@ -7,6 +7,7 @@ import { About } from './pages/about'
 import { ResetPassword } from './pages/resetPassword'
 import { Profile } from './pages/profile'
 import { Landing } from './pages/landing'
+import { SignUpEmployee } from './pages/signUpEmployee'
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         <Route path='/about' element={<About />} />
         <Route path='/resetPassword' element={<ResetPassword />}/>
         <Route path='/profile' element={<Profile />} />
+        <Route path='/signUpEmployee' element={<SignUpEmployee />} />
       </Routes>
     </>
   );
--- FrontendMicroservices/src/pages/landing.js
+++ FrontendMicroservices/src/pages/landing.js
@@ -29,6 +29,19 @@ export const Landing = ()=>{
             document.getElementById('landingpae_div_hidden').className='landingpae_div_hidden'
         }, 1300)
     }
+    const handleJoinWithUsSignUpClick=()=>{
+        document.getElementById('image1').className='landing_container_two_changeAnimation1'
+        document.getElementById('image2').className='landing_container_two_changeAnimation2'
+        document.getElementById('image3').className='landing_container_two_changeAnimation3'
+        document.getElementById('image4').className='landing_container_two_changeAnimation4'
+        document.getElementById('image5').className='landing_container_two_changeAnimation5'
+        document.getElementById('image6').className='landing_container_two_changeAnimation6'
+        document.getElementById('landing_container_three').className='landing_container_three_changeAnimation'
+        document.getElementById('center_div').className='landing_container_centerdiv_changedAnimation'
+        setTimeout(()=>{
+            navigate('/signUpEmployee')
+        },1400)
+    }
     return(
         <div className="landing_container_one">
             <div style={{position:"absolute",top:'0', zIndex:'1'}} className='main_container_animation'>
@@ -57,8 +70,10 @@ export const Landing = ()=>{
                 <button className="landingpage_button" onClick={handleExploreFashionClick}>Explore Fashion 🤓</button>
                 <button className="landingpage_button" onMouseOver={handleJoinWithUsMouseMove} onMouseOut={handleJoinWithUsMouseOut}>Join With Us 🤝</button>
                 <div className='landingpae_div_hidden' id='landingpae_div_hidden'>
+                    <hr style={{width:"50px",position:"absolute",marginLeft:"-250px",marginTop:"-30px",transform:"rotate(-45deg)",height:"2px"}}></hr>
                     <button className="landingpae_div_hidden_button" >Login🤓</button><br></br>
-                    <button className="landingpae_div_hidden_button" >SignUp🛂</button>       
+                    <hr style={{width:"50px",position:"absolute",marginLeft:"-250px",marginTop:"40px",transform:"rotate(45deg)",height:"2px"}}></hr>
+                    <button className="landingpae_div_hidden_button" onClick={handleJoinWithUsSignUpClick}>SignUp🛂</button>
                 </div>
             </center>
         </div>
